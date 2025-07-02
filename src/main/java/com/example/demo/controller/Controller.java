a1418dcc6265c9f8892e7abb2dff65d4c168a9f6
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2025-07-02 19:43:43 -0400

    git route made

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,4 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "Worlds") String name){
+
+        return String.format("Hello %s!", name);
+    }
+
 }
